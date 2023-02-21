deb3329146855aa2933f19420a370ad664e72ae6
Yunilda Justo <105021692+Yunilda09@users.noreply.github.com>
2023-02-20 23:23:00 -0400

    GestionInventarioApi

---
+++ Data/Contexto.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using GestionInventarioApi.Models;
+
+namespace GestionInventarioApi.Data
+{
+    public class Contexto: DbContext
+    {
+        public DbSet<Articulos> Articulos => Set<Articulos>();
+
+
+        public Contexto(DbContextOptions<Contexto> options)
+            : base(options) { }
+    }
+}
---
+++ Migrations/20230221010313_initial.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GestionInventarioApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Articulos",
+                columns: table => new
+                {
+                    AriticuloId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Descripcion = table.Column<string>(type: "TEXT", nullable: true),
+                    Marca = table.Column<string>(type: "TEXT", nullable: true),
+                    Precio = table.Column<double>(type: "REAL", nullable: false),
+                    Existencia = table.Column<double>(type: "REAL", nullable: false),
+                    ImagenUrl = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Articulos", x => x.AriticuloId);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Articulos");
+        }
+    }
+}
